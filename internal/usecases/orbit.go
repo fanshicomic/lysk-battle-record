b7607038fa002acab678cbb5a60143804337dabc
fanshicomic <fanshicomic@gmail.com>
2025-09-02 19:12:17 +0800

    fix: for create as well

--- internal/usecases/orbit.go
+++ internal/usecases/orbit.go
@@ -131,7 +131,7 @@ func (s *LyskServer) ProcessOrbitRecord(c *gin.Context) {
 	record.Weapon = pkg.GetValue(input, "武器")
 	record.TotalLevel = pkg.GetValue(input, "卡总等级")
 	record.Note = pkg.GetValue(input, "备注")
-	record.StarRank = pkg.GetValue(input, "星级")
+	record.StarRank = cleanUpStarRankValue(pkg.GetValue(input, "星级"))
 
 	if _, err := record.ValidateOrbit(); err != nil {
 		logrus.Errorf("[Orbit] Record validation failed: %v", err)
